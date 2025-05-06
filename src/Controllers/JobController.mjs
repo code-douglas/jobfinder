c6d1cbf43368bb49931e2a508101c6cbd9abfd59
codin <douglasd3v.contato@gmail.com>
2025-05-06 16:31:22 -0300

    Controller e método para criação de trabalhos na plataforma

---
+++ src/Controllers/JobController.mjs
@@ -0,0 +1,22 @@
+import Job from '../Models/Job.mjs';
+
+export default class JobController {
+  static async createAJob(req, res) {
+    const { title, description, salary, company, email, new_job } = req.body;
+
+    try {
+      await Job.create({
+        title,
+        description,
+        salary,
+        company,
+        email,
+        new_job,
+      });
+
+      return res.redirect('/');
+    } catch (error) {
+      return res.status(400).send('Erro ao criar vaga.');
+    }
+  }
+}
