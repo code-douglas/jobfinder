6b0a2e165ab608c72f64f117c315b87387b1a4cb
codin <douglasd3v.contato@gmail.com>
2025-05-06 11:09:27 -0300

    Adicionando trycatch na conexão para casos de erro

--- src/app.mjs
+++ src/app.mjs
@@ -1,13 +1,21 @@
 import './config/configEnv.mjs';
+import sequelize from './db/connection.mjs';
 import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Chegou na home.');
-});
+const startApp = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Successfully connected to the database');
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+};
+
+startApp();
